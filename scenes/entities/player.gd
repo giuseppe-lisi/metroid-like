3b07e8e0212a4c0043ffdc7f2abc77b488319d8f
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-07 15:12:36 +0100

    first commit

---
+++ scenes/entities/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var direction_x: float
+var speed: int = 50
+@export var jump_strenght := 10
+@export var gravity := 10
+
+func get_input():
+	direction_x = Input.get_axis("left","right")
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_strenght
+
+func apply_gravity(delta):
+	velocity.y += gravity * delta
+
+func shooting():
+	if Input.is_action_just_pressed("shoot") and $ReloadTimer.time_left == 0:
+		$ReloadTimer.start()
+		print("shoot")
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	apply_gravity(delta)
+	shooting()
+	velocity.x = direction_x * speed
+	move_and_slide()
